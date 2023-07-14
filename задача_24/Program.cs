11c011313b8366c2957fef91c66f4709e36f99e6
«Anna» <anna.kostukova@gmail.com>
2023-07-14 22:44:36 +0500

    задача_24

---
+++ задача_24/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Promt(string message) // создаем метод для печати
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void GetSum(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum += i;  // sum = sum + 1;
+    }
+
+    Console.WriteLine($"Сумма чисел от 1 до {a} = {sum}");
+}
+int A = Promt("Введите число ");
+GetSum(A);
+
+int B = GetNumber("Введите второе число: ");
+GetSumNumber(B);
