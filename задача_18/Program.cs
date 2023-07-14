a562d82c668689d6782834c182366397ad295dbf
«Anna» <anna.kostukova@gmail.com>
2023-07-14 17:53:20 +0500

    задача_18

---
+++ задача_18/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+int Promt(string message) // создаем метод для печати
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void Find(int a)
+{
+    if (a < 1 || a > 4)
+        Console.Write("Неверный формат данных");
+    if (a == 1)
+        Console.Write("X = (0; +n), Y = (0; +n)");
+    if (a == 2)
+        Console.Write("X = (0; -n), Y = (0; +n)");
+    if (a == 3)
+        Console.Write("X = (0; -n), Y = (0; -n)");
+    if (a == 4)
+        Console.Write("X = (0; +n), Y = (0; -n)");
+}
+
+int quarter = Promt("Введите номер четверти: ");
+Find(quarter);
