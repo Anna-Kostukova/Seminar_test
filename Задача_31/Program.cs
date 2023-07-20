bc917691a2c25af0f0df896c8f18f113a77ded4e
«Anna» <anna.kostukova@gmail.com>
2023-07-20 17:49:06 +0500

    задача_31_доработан

--- Задача_31/Program.cs
+++ Задача_31/Program.cs
@@ -5,47 +5,53 @@
 // положительных чисел равна 29, сумма отрицательных равна
 // -20.
 
-int sizeArray = GetNum("Введите длину массива: ");
-int min = GetNum("Введите минимальное значение диапазона для элементов массива: ");
-int max = GetNum("Введите максимальное значение диапазона для элементов массива: ");
+int sizeArray = GetNum("Введите длину массива: "); // создаем длину (размер)
+int min = GetNum("Введите минимальное значение диапазона для элементов массива: ");  // создаем мин значение
+int max = GetNum("Введите максимальное значение диапазона для элементов массива: ");// создаем макс значение
 
-int[] array = GetArray(sizeArray, min, max);
-PrintArray(array);
+int[] array = GetArray(sizeArray, min, max); // новый массив array который вызывает GetArray
 
-Console.WriteLine($"\nсумма положительных чисел = {positiveSum} сумма отрицательных чисел = {negativeSum}");
+PrintArray(array);     // выводит на печать массив
+int[] resSum = GetSum(array);
+Console.WriteLine($"\nсумма положительных чисел = {resSum[0]} \nсумма отрицательных чисел = {resSum[1]}");
                     // \n - перенос на новую строку
 
-int GetNum(string message)
+int GetNum(string message) // метод работы с пользователем (на получение какого-то числа), 
 {
-    Console.Write(message);
-    int num = int.Parse(Console.ReadLine());
-    return num;
+    Console.Write(message);                   // сообщение выводим на консоль
+    int num = int.Parse(Console.ReadLine()); // создаем число, запрашиваем у пользователя
+    return num;                             // возвращаем наше число
 }
 
-int[] GetArray(int size, int minValue, int maxValue) // будет массив, определенного размера с заданным диапазоном
+int[] GetArray(int size, int minValue, int maxValue) // будет массив GetArray определенного размера
+                                                    // с заданным диапазоном
 {
-    int[] result = new int[size];
-    for (int i = 0; i < size; i++)
+    int[] result = new int[size];   // место хранения result 
+    for (int i = 0; i < size; i++) // пока i меньше size
     {
-        result[i] = new Random().Next(minValue, maxValue);
+        result[i] = new Random().Next(minValue, maxValue); // заполняем массив рандомными значениями
     }
-    return result;
+    return result;                                        // так как метод возвращаемый
 }
 
-void PrintArray(int[] arr)
+void PrintArray(int[] arr) // создаем метод печати PrintArray, который получит какой-то массив arr
 {
-    for (int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++) //  мы возьмем его длинну
     {
-        Console.Write($"{arr[i]} ");
+        Console.Write($"{arr[i]} ");    // и все элементы распечатаем
     }
 }
 
-int positiveSum = 0;
-int negativeSum = 0;
-
-foreach (int el in array)
+int[] GetSum(int[] arr)
 {
-    if(el > 0) positiveSum += el;
-    if(el < 0) negativeSum += el;
+    int positiveSum = 0;  // одна переменная складывает положительные значения
+    int negativeSum = 0; // вторая переменная складывает все отрицательные значения
+    int[] res = new int[2];
+    foreach (int el in arr) // цикл foreach записывает все элементы в el пока все элементы будут 
+                            //идти по очереди по всему массиву - мы находимся в цикле 
+    {
+        if (el > 0) res[0] += el;
+        if (el < 0) res[1] += el;
+    }
+    return res;
 }
-
